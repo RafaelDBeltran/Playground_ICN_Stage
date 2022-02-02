26b1c22bbe579dd15007bd383b02169a4ed9c442
RafaelDBeltran <rafaelbeltran.aluno@unipampa.edu.br>
2022-02-02 00:36:42 -0300

    1ª versão funcional do play icn-stage

---
+++ config_requirements.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get -y install git
+sudo apt-get install default-jre
+#Instasll NDN
+#Begin
+export SKIPSUDOCHECK=1
+export DEFAULT_HOME='/home/vagrant/'
+
+git clone https://github.com/named-data/mini-ndn.git
+cd mini-ndn
+
+./install.sh -y
+#End
+
+
+#Instasll NDN
+#Begin
+cd
+git clone https://github.com/named-data/ndn-traffic-generator.git
+cd ndn-traffic-generator
+./waf configure
+./waf
+sudo ./waf install
+#End
+
+#Install ICN-stage
+#Begin
+cd /home/vagrant/
+git clone https://github.com/RafaelDBeltran/icn-stage.git
+cd icn-stage
+git checkout vagrant
+pip install -r requirements.txt
+
+#End
